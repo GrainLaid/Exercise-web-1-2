4c72e9ca58d25632ff7e092a9b5cfb2cd70eb1f3
Alexsandr <alex13777@gmail.com>
2019-08-12 17:31:24 +0600

    Добавлен html код для ввода параметров

--- EE/src/HelloServlet.java
+++ EE/src/HelloServlet.java
@@ -6,11 +6,27 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
 
+
 @WebServlet("/Hello")
 
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String one = req.getParameter(" one ");
+        String two = req.getParameter(" two ");
+        resp.getWriter().write("<html>" +
+                "<head></head>" +
+                "<body>" +
+                "one = " + one +
+                "two = " + two +
+                "<form action='Hello' method='get'>" +
+                        "<input type='text' name ='one'/>" +
+                        "<input type='text' name ='two'/>" +
+                        "<input type='submit' name ='submit'/>" +
+                "</form>" +
+                "</body>" +
+                "</html>" );
+
         Enumeration<String> parametrName = req.getParameterNames();
         while (parametrName.hasMoreElements()){
             String elementName = parametrName.nextElement();
